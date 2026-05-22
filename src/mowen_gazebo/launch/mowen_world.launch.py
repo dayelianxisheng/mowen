58879bf9c7f3987b54539b2db288da6d59e69e77
dayelianxisheng <dayelianxisheng@users.noreply.github.com>
2026-05-22 22:06:52 +0800

    完成slam

--- src/mowen_gazebo/launch/mowen_world.launch.py
+++ src/mowen_gazebo/launch/mowen_world.launch.py
@@ -1,49 +1,68 @@
 import os
+
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 
+
 def generate_launch_description():
-    launch_file_dir = os.path.join(
-        get_package_share_directory('mowen_gazebo'),'launch'
-    )
-    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    pkg_mowen = get_package_share_directory('mowen_gazebo')
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     x_pose = LaunchConfiguration('x_pose', default='0.0')
     y_pose = LaunchConfiguration('y_pose', default='0.0')
 
-    world = os.path.join(
-        get_package_share_directory('mowen_gazebo'),
-        'worlds',
-        'empty_world.world')
-    
-    gzserver_cmd =IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')),
-        launch_arguments={'world': world}.items())
-    
-    gzclient_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')))
-    
+    world = os.path.join(pkg_mowen, 'worlds', 'mowen_world.world')
+
+    gazebo_plugin_path = os.pathsep.join([
+        '/opt/ros/humble/lib',
+        '/usr/lib/x86_64-linux-gnu/gazebo-11/plugins',
+    ])
+    gazebo_resource_path = os.pathsep.join([
+        '/opt/ros/humble/share/gazebo_ros',
+        '/usr/share/gazebo-11',
+    ])
+    gazebo_model_path = os.pathsep.join([
+        os.path.join(pkg_mowen, 'models'),
+        '/opt/ros/humble/share/gazebo_ros/models',
+        '/usr/share/gazebo-11/models',
+    ])
+
+    gazebo_env = {
+        'GAZEBO_MODEL_PATH': gazebo_model_path,
+        'GAZEBO_PLUGIN_PATH': gazebo_plugin_path,
+        'GAZEBO_RESOURCE_PATH': gazebo_resource_path,
+    }
+
+    gzserver_cmd = ExecuteProcess(
+        cmd=['gzserver', '--verbose', world,
+             '-s', 'libgazebo_ros_init.so',
+             '-s', 'libgazebo_ros_factory.so',
+             '-s', 'libgazebo_ros_force_system.so'],
+        output='screen',
+        additional_env=gazebo_env,
+    )
+
+    gzclient_cmd = ExecuteProcess(
+        cmd=['gzclient'],
+        output='screen',
+        additional_env=gazebo_env,
+    )
+
     robot_state_publisher_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(launch_file_dir,'robot_state_publisher.launch.py')),
-        launch_arguments={'use_sim_time': use_sim_time}.items()
-    )
-    
+            os.path.join(pkg_mowen, 'launch', 'robot_state_publisher.launch.py')),
+        launch_arguments={'use_sim_time': use_sim_time}.items())
+
     spawn_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(launch_file_dir, 'spawn_mowen.launch.py')),
-            launch_arguments={
-                'x_pose':x_pose,
-                'y_pose':y_pose
-            }.items())
-    
+            os.path.join(pkg_mowen, 'launch', 'spawn_mowen.launch.py')),
+        launch_arguments={'x_pose': x_pose, 'y_pose': y_pose}.items())
+
     ld = LaunchDescription()
+    ld.add_action(DeclareLaunchArgument('use_sim_time', default_value='true'))
     ld.add_action(gzserver_cmd)
     ld.add_action(gzclient_cmd)
     ld.add_action(robot_state_publisher_cmd)
